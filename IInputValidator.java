850b59d1a7f6f2c809787f3d5e9be079cac97e31
cholowerrr <135770248+cholowerrr@users.noreply.github.com>
2024-10-24 23:32:40 +0800

    Delete IInputValidator.java

--- IInputValidator.java
+++
@@ -1,6 +0,0 @@
-public interface IInputValidator {
-    int getValidIntInput(String prompt);
-    double getValidDoubleInput(String prompt);
-    String getValidStringInput(String prompt);
-    String getValidEmployeeType();
-}
